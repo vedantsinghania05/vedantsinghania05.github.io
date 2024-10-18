567fe4a7b0ef3294f3588ff157527816df53de94
Naya Singhania <naya.singhania@gmail.com>
2024-10-18 16:50:07 -0700

    update projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import sitemap from "@astrojs/sitemap";
 
 import partytown from "@astrojs/partytown";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.nayasinghania.com",
